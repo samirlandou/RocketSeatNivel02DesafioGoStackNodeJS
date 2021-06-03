3b364503f3f0f6993a5878bb364561f4c498e407
Samir Landou <samirlandou@gmail.com>
2021-06-03 11:57:09 -0300

    :Sparkles: Transaction challenge finalized

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,18 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+/**
+ * [x] Recebimento das informações
+ * [x] Tratativa de erros/excussões
+ * [x] Acesso ao repositório
+ */
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +20,22 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Transaction type is invalid!');
+    }
+
+    const { total } = this.transactionsRepository.getBalance();
+    if (type === 'outcome' && total <= value) {
+      throw new Error('You do not have enough balance!');
+    }
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
